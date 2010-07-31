6113a34d940cf328d2653953d5e2607713e83a42
Aiden Nibali <dismaldenizen@gmail.com>
2010-07-31 19:52:00 +1000

    improved syntax error messages

--- lib/coal/parser.rb
+++ lib/coal/parser.rb
@@ -7,10 +7,20 @@ module Coal
       new.parse(*args).tree
     end
     
+    # Parse Coal code into a translator-friendly tree representation.
+    #
+    # @return [Array] the parse tree.
     def parse *args
       res = super
       if res.nil?
-        raise SyntaxError.new failure_reason
+        message = ""
+        line = args.first.lines.to_a[failure_line - 1].strip
+        col = failure_column - 1
+        message << "Parsing failed on line #{failure_line}\n"
+        message << "  " + line + "\n  "
+        message << " " * [col - 1, 0].max + (col.zero? ? "^^" : "^^^") + "\n"
+        message << failure_reason
+        raise SyntaxError.new(message)
       end
       res
     end
