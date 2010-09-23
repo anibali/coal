b4d9fec1cb126e0b7623413680a4d20b9d36829f
Aiden Nibali <dismaldenizen@gmail.com>
2010-09-23 18:39:58 +1000

    adjustments to match new handling of stringz values in libjit-ffi

--- lib/cl/glib/gquark.rb
+++ lib/cl/glib/gquark.rb
@@ -22,24 +22,7 @@ Coal.module "GLib" do |m|
   end
 end
 
-# TODO: make dealing with strings agnostic of translator
-class Cl::GLib::GQuark
-  alias_method :old_to_sz, :to_sz
-  
-  def to_sz
-    old_to_sz.get_string(0)
-  end
-  
-  class << self
-    alias_method :old_new, :new
-    
-    def new str
-      old_new(FFI::MemoryPointer.from_string str)
-    end
-  end
-end
-
-#q = Cl::GLib::GQuark.new('hello')
-#p q.to_sz
-#q.destroy
+q = Cl::GLib::GQuark.new('hello')
+puts q.to_sz
+q.destroy
 
