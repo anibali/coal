c3b53ee8dbfc62dfb5fd2989b30d014022f5d54e
Aiden Nibali <dismaldenizen@gmail.com>
2010-11-19 12:18:24 +1100

    Added more stdio functions

--- lib/coal/translators/libjit/includes.rb
+++ lib/coal/translators/libjit/includes.rb
@@ -2,16 +2,33 @@ module Coal::Translators ; LibJIT.class_eval do
   
   class Includes
     def self.add trans, name
+      @translator = trans
       case name
       when 'math.h'
-        trans.add_special_function('pow') { |f, a, b| a ** b }
-        trans.add_special_function('powf') { |f, a, b| a ** b }
+        asf('pow') { |f, a, b| a ** b }
+        asf('powf') { |f, a, b| a ** b }
         # ...
       when 'stdio.h'
-        trans.add_special_function('printf') { |f, *args| f.c.printf *args }
-        # ...
+        libc *%w[remove rename tmpfile, tmpnam]
+        libc *%w[fclose fflush fopen freopen setbuf] #setvbuf
+        libc *%w[fprintf fscanf printf scanf sprintf sscanf] #snprintf, vprintf...
+        libc *%w[fgetc fgets fputc fputs getc getchar gets putc putchar puts ungetc]
+        #fread, fwrite
+        libc *%w[fgetpos fseek fsetpos ftell rewind]
+        libc *%w[clearerr feof ferror perror]
       else
         raise "missing include: #{name}" if inc.nil?
+      end
+    end
+    
+    private
+    def self.asf(*args, &block)
+      @translator.add_special_function *args, &block
+    end
+    
+    def self.libc *names
+      names.each do |name|
+        asf(name) { |f, *args| f.c.call_native(name, *args) }
       end
     end
   end
