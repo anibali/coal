532f6f9f6aacf2a490b51f96bf5b14e5797f0025
Aiden Nibali <dismaldenizen@gmail.com>
2010-11-20 14:56:34 +1100

    Added stdlib functions

--- lib/coal/translators/libjit/includes.rb
+++ lib/coal/translators/libjit/includes.rb
@@ -9,13 +9,38 @@ module Coal::Translators ; LibJIT.class_eval do
         asf('powf') { |f, a, b| a ** b }
         # ...
       when 'stdio.h'
+        # Operations on files
         libc *%w[remove rename tmpfile, tmpnam]
-        libc *%w[fclose fflush fopen freopen setbuf] #setvbuf
-        libc *%w[fprintf fscanf printf scanf sprintf sscanf] #snprintf, vprintf...
+        # File access functions
+        libc *%w[fclose fflush fopen freopen setbuf setvbuf]
+        # Formatted input/output functions
+        libc *%w[fprintf fscanf printf scanf sprintf sscanf snprintf] #vprintf...
+        # Character input/output functions
         libc *%w[fgetc fgets fputc fputs getc getchar gets putc putchar puts ungetc]
-        #fread, fwrite
+        # Direct input/output functions
+        libc *%w[fread, fwrite]
+        # File positioning functions
         libc *%w[fgetpos fseek fsetpos ftell rewind]
+        # Error-handling functions
         libc *%w[clearerr feof ferror perror]
+      when 'stdlib.h'
+        # Numeric conversion functions
+        libc *%w[atof atoi atol atoll strtod strtof 
+          strtol strtoll strtoul strtoull] #strtold
+        # Pseudo-random sequence generation functions
+        libc *%w[rand srand]
+        # Memory management functions
+        libc *%w[calloc free malloc realloc]
+        # Communication with the environment
+        libc *%w[abort exit _Exit getenv system] #atexit
+        # Searching and sorting utilities
+        libc *%w[bsearch qsort]
+        # Integer arithmetic functions
+        libc *%w[abs labs llabs] #div, ldiv, lldiv
+        # Multibyte/wide character conversion functions
+        libc *%w[mblen] #mbtowc, wctomb
+        # Multibyte/wide string conversion functions
+        #mbstowcs, wcstombs
       else
         raise "missing include: #{name}" if inc.nil?
       end
